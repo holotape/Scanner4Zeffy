82116073f767e3b9e6ed736a42295728f95c99ce
holotape <7445231+holotape@users.noreply.github.com>
2023-09-29 00:03:16 -0400

    Working on camera display issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,30 +51,33 @@ android {
 
 dependencies {
 
+    // Core and AppCompat
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // CameraX
     implementation("androidx.camera:camera-core:1.2.3")
-    implementation("androidx.camera:camera-core:1.1.0")
-    implementation("androidx.camera:camera-camera2:1.0.0")
-    implementation("androidx.camera:camera-lifecycle:1.1.0")
-    implementation("androidx.camera:camera-view:1.0.0-alpha30")
+    implementation("androidx.camera:camera-camera2:1.2.3")
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-view:1.2.3")
+
+    // ZXing for QR
     implementation("com.google.zxing:core:3.4.1")
-    implementation("androidx.appcompat:appcompat:1.6.1") // For QR code
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation ("androidx.camera:camera-core:1.1.0")
-    implementation ("androidx.camera:camera-camera2:1.1.0")
-    implementation ("androidx.camera:camera-view:1.0.0-alpha30")
-
+    androidTestImplementation("androidx.compose.ui:ui:1.5.2")
+    debugImplementation("androidx.compose.ui:ui-test:1.5.2")
 }
--- app/src/main/java/com/example/scanner4zeffy/MainActivity.kt
+++ app/src/main/java/com/example/scanner4zeffy/MainActivity.kt
@@ -1,49 +1,114 @@
 package com.example.scanner4zeffy
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.MultiFormatReader
 import com.google.zxing.NotFoundException
 import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.common.HybridBinarizer
-import androidx.camera.view.PreviewView
-
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    // ...lifecycle methods
+    private lateinit var cameraExecutor: ExecutorService
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_CAMERA = 1
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  // Load classic XML layout
-        // Init PreviewView
-        val previewView: PreviewView = findViewById(R.id.preview_view)
+        setContentView(R.layout.activity_main)
 
-        // Initialize WebView
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         val webView: WebView = findViewById(R.id.web_view)
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
+        val previewView: PreviewView = findViewById(R.id.preview_view)
+        startCamera(previewView)
 
-        // Initialize Camera
-        startCamera()
     }
 
-    private fun startCamera() {
-        // Setup image analysis to scan QR codes
+    private fun startCamera(previewView: PreviewView) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "We need camera access to scan QR codes", Toast.LENGTH_LONG).show()
+            }
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA),
+                MY_PERMISSIONS_REQUEST_CAMERA)
+            return
+        }
+
         val imageAnalysis = ImageAnalysis.Builder()
             .build()
             .also {
                 it.setAnalyzer(ContextCompat.getMainExecutor(this), QRCodeAnalyzer { url ->
-                    // This will be called when a QR code is detected
                     val webView: WebView = findViewById(R.id.web_view)
                     webView.loadUrl(url)
                 })
             }
 
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            cameraProvider.unbindAll()
+
+            try {
+                cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalysis)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CAMERA -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    val previewView: PreviewView = findViewById(R.id.preview_view)
+                    startCamera(previewView)  // <-- Here you call startCamera without the previewView argument
+                } else {
+                    // Permission was denied. Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "Camera permission is required to use the scanner", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                // Handle other permissions if any
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
 class QRCodeAnalyzer(private val onQRCodeDetected: (String) -> Unit) : ImageAnalysis.Analyzer {
@@ -77,3 +142,5 @@ class QRCodeAnalyzer(private val onQRCodeDetected: (String) -> Unit) : ImageAnal
         }
     }
 }
+
+
