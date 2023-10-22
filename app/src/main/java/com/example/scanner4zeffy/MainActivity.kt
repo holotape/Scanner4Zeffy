f5f35143e33fe5d6a57885e1a661e5e312b57152
holotape <7445231+holotape@users.noreply.github.com>
2023-10-22 00:13:34 -0400

    1.0 Release Build

--- app/src/main/java/com/example/scanner4zeffy/MainActivity.kt
+++ app/src/main/java/com/example/scanner4zeffy/MainActivity.kt
@@ -42,10 +42,10 @@ class MainActivity : AppCompatActivity() {
         val mediaPlayer = MediaPlayer.create(this, resourceId)
         mediaPlayer.setOnCompletionListener {
             it.release()
-            Log.d(TAG, "Media player released.")
+//          Log.d(TAG, "Media player released.")
     }
-        mediaPlayer.start()  // This will start the media player immediately after creation
-        Log.d(TAG, "Attempting to play sound.")
+        mediaPlayer.start()
+//      Log.d(TAG, "Attempting to play sound.")
 }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,7 +61,7 @@ class MainActivity : AppCompatActivity() {
                 super.onPageFinished(view, url)
 
                 webView.evaluateJavascript("document.body.innerText") { result ->
-                    // check for specific text in the html
+                    // check for specific text in the html document
                     if (result.contains("Already Validated")) {
                         playSound(R.raw.chip_negative)
                     }
@@ -106,7 +106,6 @@ class MainActivity : AppCompatActivity() {
                     },
                     playSound = ::playSound,
                         rebuildQueue = ::rebuildImageAnalysisQueueDepth // Pass the function to rebuild the queue
-                    // imageAnalysis = it // Pass the ImageAnalysis instance
                 )
             )
         }
@@ -137,14 +136,13 @@ class MainActivity : AppCompatActivity() {
             MY_PERMISSIONS_REQUEST_CAMERA -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     val previewView: PreviewView = findViewById(R.id.preview_view)
-                    startCamera(previewView)  // <-- Here you call startCamera without the previewView argument
+                    startCamera(previewView)
                 } else {
                     // Permission was denied. Disable the functionality that depends on this permission.
                     Toast.makeText(this, "Camera permission is required to use the scanner", Toast.LENGTH_SHORT).show()
                 }
             }
             else -> {
-                // Handle other permissions if any
             }
         }
     }
@@ -175,15 +173,10 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-       // val preview = Preview.Builder().build().also {
-     //       val previewView: PreviewView = findViewById(R.id.preview_view)
-     //       it.setSurfaceProvider(previewView.surfaceProvider)
-     //   }
-
         try {
             cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, newImageAnalysis)
         } catch (exc: Exception) {
-            Log.e(TAG, "Use case binding failed", exc)
+//          Log.e(TAG, "Use case binding failed", exc)
         }
     }
 
@@ -192,13 +185,11 @@ class QRCodeAnalyzer(
     private val onQRCodeDetected: (String) -> Unit,
     private val playSound: (Int) -> Unit,
     private val rebuildQueue: (Int) -> Unit,
-    //private val imageAnalysis: ImageAnalysis
 ) : ImageAnalysis.Analyzer {
 
     private var isCooldown = false
     private var frameCounter = 0
     private val reader = MultiFormatReader()
-
 
     companion object {
         private const val TAG = "QRCodeAnalyzer"
@@ -210,7 +201,6 @@ class QRCodeAnalyzer(
                 isCooldown = false
                 frameCounter = 0
                 rebuildQueue(3)
-                //imageAnalysis.setImageQueueDepth(3)  // set back to normal value
             }
             image.close()
             return
@@ -243,8 +233,7 @@ class QRCodeAnalyzer(
                 Log.d(TAG, "QR code detected: ${result.text}")  // debug
                 isCooldown = true
                 frameCounter = 0
-                rebuildQueue(60)  // Set to a queue depth of 30
-                //imageAnalysis.setImageQueueDepth(30)  // set to higher value during cool-down
+                rebuildQueue(60)  // Set to a queue depth of 60
             }
 
         } catch (e: NotFoundException) {
